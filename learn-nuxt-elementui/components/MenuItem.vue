a5a7817bb8ce470b274bd40c441f00a58110f7cf
藤野 勇輝@DES <fujino-yuki@dmm.com>
2018-08-16 20:03:28 +0900

    first-commit

---
+++ learn-nuxt-elementui/components/MenuItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <li class="menuItem">{{ menuValue }}</li>
+</template>
+
+<script>
+export default {
+    props: ['menuValue']
+}
+</script>
+
+<style scoped lang="scss">
+  .menuItem {
+      color: blue;
+  }
+</style>
+
---
+++ learn-nuxt-elementui/pages/test01.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+      <menu-item menuValue="hello!"></menu-item>
+      <ul>
+          <menu-item v-for="list in lists" :key="list.value" v-bind:menuValue="list"></menu-item>
+          <!--<li class="test" v-for="list in lists" :key="list.value">{{list}}</li>-->
+      </ul>
+  </section>
+</template>
+
+<script>
+import MenuItem from "~/components/MenuItem.vue";
+
+export default {
+  components: {
+    MenuItem
+  },
+  data: () => {
+    return {
+      lists: ["hello1", "hello2", "hello3", "hello4"]
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
+
