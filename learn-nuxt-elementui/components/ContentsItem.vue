029901183580270cce0ba0f0a289c15cfd52dcd4
藤野 勇輝@DES <fujino-yuki@dmm.com>
2018-08-17 20:47:35 +0900

    [wip] save

---
+++ learn-nuxt-elementui/components/ContentsItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>{{ item }}</li>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  created() {
+    console.log('ContentsItem_hello');
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  li {
+      list-style: none;
+      color: blue;
+  }
+</style>
+
--- learn-nuxt-elementui/nuxt.config.js
+++ learn-nuxt-elementui/nuxt.config.js
@@ -34,6 +34,7 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  srcDir: ['static/','pages/'],
 }
 
